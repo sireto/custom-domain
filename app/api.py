67c41e695f3b75e1f7df8e0725e27eeccfea350e
Sandip Pandey <sandip@sireto.com>
2023-05-06 11:37:09 +0200

    Clean up Domain API

--- app/api.py
+++ app/api.py
@@ -9,30 +9,28 @@ from app.security import get_api_key
 """
 Domain API
 ===========
-GET     /domains?host=domain&port=443
-POST    /domains?host=domain&port=443 
-DELETE  /domains?host=domain&port=443
+GET     /domains
+POST    /domains?domain=<domain>&upstream=<upstream> 
+DELETE  /domains?domain=<domain>
 """
 domain_api = APIRouter()
 
 
-@domain_api.get("/domains", tags=["Domain API"])
+@domain_api.get("/domains", tags=["Custom Domain API"])
 async def get_domains(api_key: APIKey = Depends(get_api_key)):
     return caddy_server.list_domains()
 
 
-@domain_api.get("/domains/config", tags=["Domain API"])
-async def get_config(api_key: APIKey = Depends(get_api_key)):
-    return caddy_server.deployed_config()
-
-
-@domain_api.post("/domains", tags=["Domain API"])
-async def add_domain(domain: str, upstream: Optional[str] = None, api_key: APIKey = Depends(get_api_key)):
+@domain_api.post("/domains", tags=["Custom Domain API"])
+async def add_domain(domain: str,
+                     upstream: Optional[str] = None,
+                     api_key: APIKey = Depends(get_api_key)):
     caddy_server.add_custom_domain(domain, upstream)
     return "OK"
 
 
-@domain_api.delete("/domains", tags=["Domain API"])
-async def remove_domains(host: str, api_key: APIKey = Depends(get_api_key)):
-    caddy_server.remove_custom_domain(host)
+@domain_api.delete("/domains", tags=["Custom Domain API"])
+async def remove_domains(domain: str,
+                         api_key: APIKey = Depends(get_api_key)):
+    caddy_server.remove_custom_domain(domain)
     return "OK"
